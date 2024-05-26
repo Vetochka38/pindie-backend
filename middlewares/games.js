28c6f8dc94137520990f2540c5932c8eb93a32ca
Vetochka38 <veta.pavlencko@gmail.com>
2024-05-26 18:28:39 +0800

    финал

--- middlewares/games.js
+++ middlewares/games.js
@@ -114,11 +114,11 @@ const checkIfUsersAreSafe = async (req, res, next) => {
 
 const checkIsGameExists = async (req, res, next) => {
   const isInArray = req.gamesArray.find((game) => {
-    return req.body.name === game.name;
+    return req.body.title === game.title;
   });
   if (isInArray) {
     res.setHeader("Content-Type", "application/json");
-    res.status(400).send(JSON.stringify({ message: "Игра уже существует" }));
+        res.status(400).send(JSON.stringify({ message: "Игра с таким названием уже существует" }));
   } else {
     next();
   }
